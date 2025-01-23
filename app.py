d45229084165b8b15e54ba61862ec48bb7ce4372
PatrickFaedo <luanfaedo99@gmail.com>
2025-01-23 13:21:18 -0300

    Adicionando novas mudan

--- app.py
+++ app.py
@@ -126,5 +126,5 @@ def filtrar():
 def index():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
